4db5cd93a996bb34c771e5f096daee38c904fc14
kewang <cpckewang@gmail.com>
2017-12-14 12:18:27 +0800

    Complete HBase request

--- src/main/java/tw/kewang/App.java
+++ src/main/java/tw/kewang/App.java
@@ -1,7 +1,38 @@
 package tw.kewang;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class App {
+    private static String URL;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length != 1) {
+            System.out.println("Please input HBase URL");
+
+            System.exit(1);
+        }
+
+        URL = args[0];
+
+        try {
+            Document doc = Jsoup.connect(URL + "/master-status").get();
+
+            Elements tables = doc.select("#tab_userTables > table > tbody > tr > td:nth-child(2)");
+
+            for (Element table : tables) {
+                Document tableDoc = Jsoup.connect(URL + "/table.jsp?name=" + table.text()).get();
+
+                Element request = tableDoc.selectFirst("body > div.container-fluid.content > div:nth-child(2) > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(6)");
+
+                System.out.println(table.text() + " request: " + request.text());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
